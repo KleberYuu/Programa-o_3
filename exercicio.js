96340318757d3aae042cf35799e3184fdda138ad
Kleber Silva dos Santos <binho_314@hotmail.com>
2024-09-03 19:59:09 -0300

    excluir

--- exercicio.js
+++ exercicio.js
@@ -1,31 +1,142 @@
-// exercicio 01 
-console.log("olá mundo!!");
+// // exercicio 01 
+// console.log("olá mundo!!");
 
-// exercicio 02 
-function converterMetrosParaCentimetros() {
-    return metros * 100;
-}
+// // exercicio 02 
+// function converterMetrosParaCentimetros() {
+//     return metros * 100;
+// }
 
-let metros = 5;
-let centimetros = converterMetrosParaCentimetros(metros);
-console.log(`${metros} metros é igual a ${centimetros} centímetros.`);
+// let metros = 5;
+// let centimetros = converterMetrosParaCentimetros(metros);
+// console.log(`${metros} metros é igual a ${centimetros} centímetros.`);
 
-// exercicio 03
-let n1 = 5;
-let n2 = 8;
-let n3 = 7.5;
-let n4 = 10;
+// // exercicio 03
+// let n1 = 5;
+// let n2 = 8;
+// let n3 = 7.5;
+// let n4 = 10;
 
-media = (n1 + n2 + n3 + n4) / 4;
-console.log(`A média das notas ${n1}, ${n2}, ${n3}, ${n4} é = ${media.toFixed(2)}`);
+// media = (n1 + n2 + n3 + n4) / 4;
+// console.log(`A média das notas ${n1}, ${n2}, ${n3}, ${n4} é = ${media.toFixed(2)}`);
 
-// 
-function maiorNumero(n, m) {
-    if (n > m){
-        console.log(`${n} é maior que ${m}`)
-    }
-    else{
-        console.log(`${nm} é maior que ${m}`)
-    }
-}
-maiorNumero(n1,n2)
+// // 
+// function maiorNumero(n, m) {
+//     if (n > m){
+//         console.log(`${n} é maior que ${m}`)
+//     }
+//     else{
+//         console.log(`${n} é maior que ${m}`)
+//     }
+// }
+// maiorNumero(n1,n2)
+
+// //
+// function somadedoisnumeros( n1, n2){
+//     return n1 + n2;
+// }
+
+// soma = somadedoisnumeros(2, 2);
+// console.log(soma);
+//
+
+// function saudacoes(empressa){
+//     return `Seja bem vindo á ${empressa}`;
+// };
+
+// var nomeDaEmpressa = 'Americanas';
+// var nomeDaEmpressa1 = 'Amazom';
+// console.log(saudacoes(nomeDaEmpressa));
+// console.log(saudacoes(nomeDaEmpressa1));
+// //
+
+// var idade1 = 10;
+// var idade2 = 20;
+
+// function deMaiorOuMenor( idade ){
+//     if( idade>= 18 ){
+//         return console.log(`Sua idade é ${idade} e voçe é maior de idade`);
+//     } else {
+//         return console.log(`Sua idade é ${idade} e voçe é menor de idade`);
+//     }
+// };
+
+// deMaiorOuMenor(idade1);
+// deMaiorOuMenor(idade2);
+// //
+
+// var palavra1 = 'arara';
+// var palavra2 = 'carro';
+
+// function palindromo(palavra) {
+//     // Transforma a string em um array, inverte e compara com a string original
+//     var palavraInvertida = palavra.split('').reverse().join('');
+    
+//     if (palavraInvertida === palavra) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// console.log(palindromo(palavra1)); 
+// console.log(palindromo(palavra2)); 
+// //
+
+// var n1 = 8;
+// var n2 = 10;
+// var n3 = 1;
+
+// function maiorEntre(n1, n2, n3){
+//     if( n1 > n2 && n1 > n3 ){
+//         console.log(`O numero ${n1} é o maior`);
+//     } else if ( n2 > n1 && n2 > n3 ){
+//         console.log(`O numero ${n2} é o maior`);
+//     } else {
+//         console.log(`O numero ${n3} é o maior`);
+//     }
+// }
+// maiorEntre(n1, n2, n3);
+// maiorEntre(5, 10, 40);
+// maiorEntre(44, 5, 10);
+// //
+
+// Criar array esparso
+let arrayEsparso = [];
+
+// Atribuir valores aos índices 1, 3 e 7
+arrayEsparso[1] = 'Valor no índice 1';
+arrayEsparso[3] = 'Valor no índice 3';
+arrayEsparso[7] = 'Valor no índice 7';
+
+// Imprimir o array esparso
+console.log(arrayEsparso);
+//
+
+// Imprimir o comprimento do array
+console.log('Comprimento do array:', arrayEsparso.length);
+
+let array5 = [1,2,3,4,5];
+console.log(array5);
+
+array5[0] = 0;
+console.log(array5);
+//
+
+let meuArray = [];
+meuArray.push(1,2,3);
+console.log(meuArray);
+meuArray[0] = 2;
+console.log(meuArray);
+//
+
+let clinica = [];
+clinica[0] = 'cachorro';
+clinica[1] = 'gato';
+clinica[2] = 'piriquito';
+console.log(clinica);
+
+clinica[0] = 'outro cachorro';
+console.log(clinica);
+clinica[1] = 'outro gato';
+clinica[2] = 'outro piriquito';
+console.log(clinica);
---
+++ exercicio_1.js
@@ -0,0 +1,72 @@
+// const personagem = {
+//     nome: "kleber",
+//     classe: "Mago",
+//     nivel: "20",
+//     aliado: {
+//         nome: "Yuu",
+//         classe: "assassino"
+//     },
+//     status: "desaparecido"
+// }
+
+// console.log(personagem);
+// delete personagem.aliado;
+// console.log(personagem);
+
+// const livro = {
+//     titulo: "Jogando Honkai",
+//     autor: "Kleber",
+//     anoPublicacao: 2000,
+//     genero: "Ficção"
+// }
+// console.log(livro);
+
+// anoAtual = new Date().getFullYear()
+// livro.idadePublicacao = anoAtual - livro.anoPublicacao;
+// console.log(livro);
+
+// // Criando a string mostrarDetalhes
+// const mostrarDetalhes = `Título: ${livro.titulo}\nAutor: ${livro.autor}\nAno de Publicação: ${livro.anoPublicacao}\nGênero: ${livro.genero}\nIdade da Publicação: ${livro.idadePublicacao} anos`;
+// console.log(mostrarDetalhes);
+
+// livro.avaliacao = null;
+// console.log(livro);
+
+// // Verificando se avaliacao é null e atribuindo um valor
+// if (livro.avaliacao === null) {
+//     // livro.avaliacao = 4.5; // Atribuindo uma avaliação ao livro
+//     console.log("Avaliação adicionada:", livro.avaliacao);
+// } else {
+//     console.log("O livro já possui uma avaliação.");
+// }
+// console.log(livro);
+
+// delete livro.avaliacao;
+// console.log(livro);
+
+
+// const pessoa = {
+//     nome: "kleber",
+//     idade: 35,
+//     solteiro: false,
+//     hobbies: ["ver animes", "jogar gacha", "comer coisa boa"]
+// }
+
+// function mostrarInfoPessoa(pessoa) {
+//     console.log(`Nome: ${pessoa.nome} (Tipo: ${typeof pessoa.nome})`);
+//     console.log(`Idade: ${pessoa.idade} `);
+//     console.log(`Estado Civil: ${pessoa.solteiro ? "solteiro" : "casado"}`);
+//     console.log(`Hobbies: ${pessoa.hobbies.join(", ")}`);
+//   }
+  
+// // Chamando a função e passando o objeto 'pessoa' como argumento
+// mostrarInfoPessoa(pessoa);
+
+// pessoa.endereco = {
+//         rua: "algima rua ai",
+//         cidade: "Fpolis",
+//         estado: "SC"
+// }
+
+// console.log(pessoa);
+
